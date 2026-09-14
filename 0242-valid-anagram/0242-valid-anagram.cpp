f213de2eecab3c042dda3ac9c2ce96afea2332c6
neevkasliwal <neevkasliwal@gmail.com>
2026-09-14 10:36:32 +0530

    Time: 0 ms (100%), Space: 9.7 MB (48.52%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,12 +1,22 @@
 class Solution {
-public:
+public: 
+    
     bool isAnagram(string s, string t) {
+        const int SIZE = 256;
+        int count[SIZE] = {0};
         if(s.length() != t.length()){
             return false;
         }
-        sort(s.begin(),s.end());
-        sort(t.begin(),t.end());
-
-        return(s==t);
+        for(int i = 0; i<s.length();i++){
+            count[s[i]]++;
+            count[t[i]]--;
+        }
+        for(int i = 0;i < SIZE ; i++){
+            if(count[i]!=0){
+                return false;
+            }
+        }
+        return true;
+        
     }
 };
